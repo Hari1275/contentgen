c734413904da056890a6a52a96784f63ccb715ed
hari1275 <khari1275@gmail.com>
2025-06-20 23:19:20 +0530

    Fix module path and packaging

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="smart_ai",
     version="0.1",
-    packages=find_packages(),
+    packages=['smart_ai'],
+    package_dir={'smart_ai': 'app'},
     install_requires=[
         line.strip() for line in open('requirements.txt') if line.strip()
     ],
